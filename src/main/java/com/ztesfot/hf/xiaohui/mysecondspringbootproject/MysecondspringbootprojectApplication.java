2f6fb4681c502c4a66d7ae2c553730783b5ab415
dongxiaohui <dongxiaohui@haohandata.com.cn>
2019-08-25 18:48:10 +0800

    Initial commit

---
+++ src/main/java/com/ztesfot/hf/xiaohui/mysecondspringbootproject/MysecondspringbootprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ztesfot.hf.xiaohui.mysecondspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysecondspringbootprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysecondspringbootprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ztesfot/hf/xiaohui/mysecondspringbootproject/MysecondspringbootprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ztesfot.hf.xiaohui.mysecondspringbootproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MysecondspringbootprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
